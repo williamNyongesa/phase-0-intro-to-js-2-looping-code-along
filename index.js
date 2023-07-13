1a4d28a51a0d89f48b17d845687cc9a23d386774
williamNyongesa <marakawilliam@gmail.com>
2023-07-13 16:40:04 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+function writeCards(names) {
+    let thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+    let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    thankYouMessages.push(message);
+    }
+    return thankYouMessages;
+    }
+    
+    let names = ["will", "gin", "lyn"];
+    console.log(writeCards(names));
+   
+    
+    function countDown(number) {
+        for (let i = number; i >= 0; i--) {
+          console.log(i);
+        }
+      }
+      
+      module.exports = {
+        
+        
+        countDown
+      };
+    
+    
+    
+    
+    
